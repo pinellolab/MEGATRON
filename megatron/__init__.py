414f99e45ac8815885925c7a0d484d06420e1795
huidongchen <hd7chen@gmail.com>
2021-01-27 21:12:31 -0500

    add subpackages

--- megatron/__init__.py
+++ megatron/__init__.py
@@ -1,1 +1,9 @@
 """MEGA TRajectories of clONes"""
+
+from ._settings import settings
+from . import preprocessing as pp
+from . import tools as tl
+from . import plotting as pl
+from .readwrite import *
+
+__version__ = "0.1a"
