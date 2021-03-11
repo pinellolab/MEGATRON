d1e820ec671ed19d38f94e547fa87ba970f26ef9
huidongchen <hd7chen@gmail.com>
2021-03-11 11:10:46 -0500

    add faster "sort_list()"

--- megatron/tools/_utils.py
+++ megatron/tools/_utils.py
@@ -3,9 +3,9 @@
 import numpy as np
 
 
-def reorder_list(input_list,
-                 ref_list):
-    """Reorder a list based on a reference list
+def sort_list(input_list,
+              ref_list):
+    """Sort a list based on a reference list
 
     Parameters
     ----------
@@ -13,9 +13,6 @@ def reorder_list(input_list,
     Returns
     -------
     """
-    input_list = list(input_list)
-    list_index = []
-    for x in input_list:
-        list_index.append(ref_list.index(x))
-    output_list = np.array(input_list)[np.argsort(list_index)].tolist()
+    input_set = set(input_list)
+    output_list = [x for x in ref_list if x in input_set]
     return output_list
